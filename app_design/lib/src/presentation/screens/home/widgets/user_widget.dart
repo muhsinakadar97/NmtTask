47fce80f074fbac0b5ccfd1b7066ca6b842aa5ec
muhsinakadar97 <muhsinakadar97@gmail.com>
2024-12-09 16:31:44 +0530

    orientation errors fixed

---
+++ app_design/lib/src/presentation/screens/home/widgets/user_widget.dart
@@ -0,0 +1,27 @@
+import 'package:app_design/src/core/config/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class UserWidget extends StatelessWidget {
+  const UserWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 20,
+      top: 30,
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Icon(
+          Icons.person,
+          size: 40,
+          color: AppColors.buttonColor.withOpacity(0.15),
+        ),
+      ),
+    );
+  }
+}
---
+++ app_design/lib/src/presentation/screens/practice_questions/practice_questions.dart
@@ -0,0 +1,39 @@
+import 'package:app_design/src/presentation/controller/practice_questions_controller.dart';
+import 'package:app_design/src/presentation/screens/notes/widgets/custom_listtile.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PracticeQuestions extends StatelessWidget {
+  final PracticeQuestionsController notesController = Get.put(PracticeQuestionsController());
+
+   PracticeQuestions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        title: Text(
+          'Practice Questions',
+          style: GoogleFonts.raleway(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false, // Hide the back button
+      ),
+      body: ListView.builder(
+        itemCount: notesController.notesList.length,
+        itemBuilder: (context, index) {
+          final note = notesController.notesList[index];
+          return CustomListTile(
+            pdfImagePath: note['image']!, // Extract the image path from the map
+            topicName: note['topicName']!,
+            
+          );
+        },
+      ),
+    );
+  }
+}
